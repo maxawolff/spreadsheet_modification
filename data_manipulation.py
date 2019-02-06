5fe83e9440cbdbab657d374ed6f0ee0b0d8a0e87
maxawolff <maxawolff@hotmail.com>
2019-02-06 08:24:39 -0800

    added all rows into a list, giving me a list of each residents information

--- data_manipulation.py
+++ data_manipulation.py
@@ -5,4 +5,9 @@ import xlrd
 loc = 'resident_information_v2.xlsx'
 wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0)
-print(sheet.cell_value(0, 0))
+
+res_list = []
+for i in range(sheet.nrows - 1):
+    res_list.append(sheet.row_values(i + 1))
+
+
